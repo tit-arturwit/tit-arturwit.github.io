23071328f7eb2f4190d572d7ae8513408c2fee17
tit-arturwit <65291606+tit-arturwit@users.noreply.github.com>
2020-06-03 11:30:54 +0200

    Add files via upload

---
+++ githup/java/skrypt.js
@@ -0,0 +1,41 @@
+function zmien1()
+{
+	document.getElementById('foto').src="foto1.jpg";
+}
+function zmien2()
+{
+	document.getElementById('foto').src="foto2.jpg";
+}
+
+
+
+function zmien3()
+{
+	document.getElementById('foto2').src="foto3.jpg";
+}
+function zmien4()
+{
+	document.getElementById('foto2').src="foto4.jpg";
+}
+
+
+
+function zmien5()
+{
+	document.getElementById('foto3').src="foto5.jpg";
+}
+function zmien6()
+{
+	document.getElementById('foto3').src="foto6.jpg";
+}
+
+
+
+function zmien7()
+{
+	document.getElementById('foto4').src="foto7.jpg";
+}
+function zmien8()
+{
+	document.getElementById('foto4').src="foto8.jpg";
+}
